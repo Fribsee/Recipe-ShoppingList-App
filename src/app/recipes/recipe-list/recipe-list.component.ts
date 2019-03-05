02e76aad5b436ad94a889ad22bfd9c679e96b6af
fribsee <40807715+Fribsee@users.noreply.github.com>
2019-03-05 17:13:29 -0600

    basic structure for web page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Potato Leaking on Bacon', 'A thoroughly gross concoction', 
+    'https://pixabay.com/photos/broccoli-potato-casserole-cheese-1804446/')
+  ];
 
   constructor() { }
 
