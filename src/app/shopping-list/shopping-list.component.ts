f3ce1b86073fd97c4b107cfdc4c229712f3cb9f7
fribsee <40807715+Fribsee@users.noreply.github.com>
2019-03-11 13:33:26 -0500

    added ingredient model to create ingredients and added them to the shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient.model';
+import { setRootDomAdapter } from '@angular/platform-browser/src/dom/dom_adapter';
+
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient [] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Pumpkin', 1),
+    new Ingredient('Carrot', 3.5)
+  ];
+
 
   constructor() { }
 
